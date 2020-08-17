35d22564a039eb74b847ac0d9ae685e3c48a3375
gomi42 <gomi42@gmx.de>
2020-08-17 08:25:27 +0200

    introduce LabelComboBox to ensure identical layout

---
+++ Shape Converter/Shell/Controls/LabelComboBox.cs
@@ -0,0 +1,90 @@
+﻿//
+// Author:
+//   Michael Göricke
+//
+// Copyright (c) 2020
+//
+// This file is part of ShapeConverter.
+//
+// ShapeConverter is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see<http://www.gnu.org/licenses/>.
+
+using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ShapeConverter.Shell.Controls
+{
+    public class LabelComboBox : Control
+    {
+        /// <summary>
+        /// The Label
+        /// </summary>
+        public string Label
+        {
+            get => (string)GetValue(LabelProperty);
+            set => SetValue(LabelProperty, value);
+        }
+
+        /// <summary>
+        /// The LabelProperty
+        /// </summary>
+        public static readonly DependencyProperty LabelProperty =
+            DependencyProperty.Register("Label", typeof(string), typeof(LabelComboBox), new PropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// The DisplayMemberPath
+        /// </summary>
+        public string DisplayMemberPath
+        {
+            get => (string)GetValue(DisplayMemberPathProperty);
+            set => SetValue(DisplayMemberPathProperty, value);
+        }
+
+        /// <summary>
+        /// The LabelProperty
+        /// </summary>
+        public static readonly DependencyProperty DisplayMemberPathProperty =
+            DependencyProperty.Register("DisplayMemberPath", typeof(string), typeof(LabelComboBox), new PropertyMetadata(null));
+
+        /// <summary>
+        /// The ItemsSourceProperty
+        /// </summary>
+        public IEnumerable ItemsSource
+        {
+            get => (IEnumerable)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        /// <summary>
+        /// The LabelProperty
+        /// </summary>
+        public static readonly DependencyProperty ItemsSourceProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(LabelComboBox), new PropertyMetadata(null));
+
+        /// <summary>
+        /// The DisplayMemberPath
+        /// </summary>
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        /// <summary>
+        /// The DisplayMemberPathProperty
+        /// </summary>
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(LabelComboBox), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+    }
+}
