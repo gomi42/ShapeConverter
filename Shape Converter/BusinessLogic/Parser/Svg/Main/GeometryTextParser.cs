f83d61f056a4c06fa77e21cdcb88a63638c89886
gomi42 <gomi42@gmx.de>
2020-11-03 11:25:54 +0100

    Revert "."
    This reverts commit 43151b76e7f7f01c4cac135d9fa5382d1d819d97.

--- Shape Converter/BusinessLogic/Parser/Svg/Main/GeometryTextParser.cs
+++ Shape Converter/BusinessLogic/Parser/Svg/Main/GeometryTextParser.cs
@@ -238,6 +238,7 @@ namespace ShapeConverter.BusinessLogic.Parser.Svg.Main
 
             string[] fontNames = fontFamily.Split(new char[] { ',' });
             FontFamily family;
+            family = new FontFamily();
 
             foreach (string rawFontname in fontNames)
             {
